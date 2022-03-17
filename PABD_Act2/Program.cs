11727c10cac8c78226f1bdd8216b6e959cae2f17
Fachrezandi20 <andi.muh.ft20@mail.umy.ac.id>
2022-03-17 13:29:27 +0800

    Add project files.

---
+++ PABD_Act2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PABD_Act2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-5B0DFV64;database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar(50), " +
+                    "Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+                Console.ReadLine();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
